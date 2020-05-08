24d2be096885c465ca0d33bb47001ea6cd808690
jeffc00 <jeffersonjeff@gmail.com>
2020-05-08 01:26:36 -0300

    minor changes

--- themes/zork/zork.theme.bash
+++ themes/zork/zork.theme.bash
@@ -86,16 +86,16 @@ prompt() {
 
     # nice prompt
     case "`id -u`" in
-        0) PS1="${TITLEBAR}┌─$(my_ve)$(chroot)[$my_ps_root@$my_ps_host_root]$(modern_scm_prompt)$(__my_rvm_ruby_version)[${cyan}\w${normal}]$(is_vim_shell)
+        0) PS1="${TITLEBAR}┌─$(my_ve)$(chroot)[$my_ps_root${bold_cyan}@${normal}$my_ps_host_root]$(modern_scm_prompt)$(__my_rvm_ruby_version)[${cyan}\w${normal}]$(is_vim_shell)
 └─${bold_green}$ ${normal} "
         ;;
-        *) PS1="${TITLEBAR}┌─$(my_ve)$(chroot)[$my_ps_user@$my_ps_host]$(modern_scm_prompt)$(__my_rvm_ruby_version)[${cyan}\w${normal}]$(is_vim_shell)
+        *) PS1="${TITLEBAR}┌─$(my_ve)$(chroot)[$my_ps_user${bold_cyan}@${normal}$my_ps_host]$(modern_scm_prompt)$(__my_rvm_ruby_version)[${cyan}\w${normal}]$(is_vim_shell)
 └─${bold_green}$ ${normal}"
         ;;
     esac
 }
 
-PS2="└─$ "
+PS2="└─${bold_green}$ ${normal}"
 
 
 
